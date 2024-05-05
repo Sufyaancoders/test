83dc9a8699fc0d724815ecd7674d500ca5c669f3
sufyaan ahmad <sufyaanahmadx9x@gmail.com>
2024-05-05 17:02:22 +0530

    same code but diffrent

--- chapter1.js
+++ chapter1.js
@@ -3,4 +3,12 @@ const item = {
    age : "55"
 }
 console.log(item)
+/*echo "# javascript-prectice-couse" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Sufyaancoders/javascript-prectice-couse.git
+git push -u origin main*/
 console.log("hello")
+
